17fa677e46bfa7c91b41fc3059077bf7b8b68c9a
Eduardo Sebastian <esebastian@altiora.es>
2016-02-16 10:14:47 +0100

    player wins three balls

--- tennis/randori/tennis.rb
+++ tennis/randori/tennis.rb
@@ -8,7 +8,11 @@ class Player
   end
 
   def win_ball
-    @score += 15
+    if @score == 30
+      @score = 40
+    else
+      @score += 15
+    end
   end
 end
 
@@ -32,5 +36,14 @@ describe "Tennis game" do
     player.win_ball
     player_score = player.score
     expect(player_score).to eq(30)
+  end
+
+  it "player wins three balls" do
+    player = Player.new
+    player.win_ball
+    player.win_ball
+    player.win_ball
+    player_score = player.score
+    expect(player_score).to eq(40)
   end
 end
