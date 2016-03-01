0bc34231faec8ef3a1bdd63bb3c38ac46ab93042
Eduardo Sebastian <esebastian@altiora.es>
2016-03-01 10:34:31 +0100

    refactor: extract method

--- minesweeper.rb
+++ minesweeper.rb
@@ -3,8 +3,13 @@ class Minesweeper
   end
 
   def self.solve fields
-    raise ParsingException.new if fields =~ /-/
-    raise ParsingException.new if fields =~ /^0 [^0]/
+    raise ParsingException.new if invalid_input? fields
     ""
+  end
+
+  private
+
+  def self.invalid_input? input
+    input =~ /-/ || input =~ /^0 [^0]/
   end
 end
