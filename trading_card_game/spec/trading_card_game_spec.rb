8ac507400cabf0cbca6fba8f3f7a9a7d334c745d
Alvaro Bautista <alvarobp@gmail.com>
2016-04-05 09:37:33 +0200

    Play hand with card not exceeding mana cost

--- trading_card_game/spec/trading_card_game_spec.rb
+++ trading_card_game/spec/trading_card_game_spec.rb
@@ -8,8 +8,18 @@ describe 'Trading Card Game' do
     expect(damage).to eql(1)
   end
 
+  it 'plays a card that not exceds the mana cost' do
+    deck = [2, 1]
+    mana = 1
+
+    damage = play_hand(deck, mana)
+    
+    expect(damage).to eql(1)
+  end
+
 end
 
-def play_hand(deck, mana) 
-  return deck.first
+def play_hand(deck, mana)
+  card_with_mana_value = deck.detect { |card| mana == card }
+  card_with_mana_value
 end
