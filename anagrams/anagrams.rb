b8e56784bacc6a57c05a32067281fdca9467d758
Eduardo Sebastian <esebastian@altiora.es>
2016-05-17 08:36:12 +0200

    Refactor: extract method

--- anagrams/anagrams.rb
+++ anagrams/anagrams.rb
@@ -17,17 +17,18 @@ class TestAnagrams < Minitest::Test
 
 
   def anagrams(input_list)
-    occurrences = Hash.new(0)
+    occ = occurrences(input_list)
+    selected = occ.select{ |k, v| v > 1 }
+    result = selected.map{ |k, v| [k]*v }
+    result.flatten
+  end
 
+  def occurrences(input_list)
+    occurrences = Hash.new(0)
     input_list.each do |e|
       occurrences[e] = occurrences[e] + 1
     end
-
-    selected = occurrences.select{ |k, v| v > 1 }
-
-    result = selected.map{ |k, v| [k]*v }
-
-    result.flatten
+    occurrences
   end
 
 end
