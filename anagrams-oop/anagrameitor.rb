972d1544db0a7026d718122c9702a60ca1fbc124
Eduardo Sebastian <esebastian@altiora.es>
2016-05-24 10:36:59 +0200

    Reports anagrams

--- anagrams-oop/anagrameitor.rb
+++ anagrams-oop/anagrameitor.rb
@@ -1,11 +1,72 @@
 class Anagrameitor
   class Anagrams
+    def initialize
+      @list = []
+    end
+
+    def add word
+      @list << word
+    end
+
+    def include anagrams
+      words = anagrams.to_a
+      return if words.empty?
+      return if @list.any?{ |w| (w - words).empty? }
+      @list << words
+    end
+
     def to_a
-      []
+      @list
+    end
+  end
+
+  class Words
+    def initialize words
+      @words = words
+      @index = -1
+    end
+
+    def next?
+      @index < @words.size - 1
+    end
+
+    def next
+      @index += 1
+      @words[@index]
+    end
+
+    def clone
+      Words.new @words
     end
   end
 
   def self.anagrams_in word_list
-    Anagrams.new.to_a
+    words = Words.new word_list
+
+    result = Anagrams.new
+    while words.next?
+      candidate = words.next
+      anagrams = detect_anagrams_for candidate, words
+      result.include anagrams
+    end
+
+    result.to_a
+  end
+
+  def self.detect_anagrams_for candidate, words
+    result = Anagrams.new
+    words = words.clone
+    while words.next?
+      the_word = words.next
+      if anagram? the_word, candidate
+        result.add candidate
+        result.add the_word
+      end
+    end
+    result
+  end
+
+  def self.anagram? candidate, word
+    (candidate != word) && (candidate.size == word.size)
   end
 end
--- anagrams-oop/spec/anagrameitor_spec.rb
+++ anagrams-oop/spec/anagrameitor_spec.rb
@@ -3,9 +3,15 @@ require './anagrameitor'
 RSpec.describe "Anagrameitor" do
 
   it "reports no anagrams" do
-    word_list = %w(xavi batu gerard)
+    word_list = %w(xaviuzz batu gerard)
     result = Anagrameitor.anagrams_in word_list
     expect(result).to eq([])
+  end
+
+  it "reports anagrams" do
+    word_list = %w(ac b ca)
+    result = Anagrameitor.anagrams_in word_list
+    expect(result).to eq([['ac', 'ca']])
   end
 
 end
