a490155992d8c01f07d9622d0f4ba2777a3fdae0
Eduardo Sebastian <esebastian@altiora.es>
2016-05-31 09:27:29 +0200

    Refactor: add some contexts in the spec

--- anagrams-oop/spec/anagrameitor_spec.rb
+++ anagrams-oop/spec/anagrameitor_spec.rb
@@ -2,16 +2,32 @@ require './anagrameitor'
 
 RSpec.describe "Anagrameitor" do
 
-  it "reports no anagrams" do
-    word_list = %w(xaviuzz batu gerard)
-    result = Anagrameitor.anagrams_in word_list
-    expect(result).to eq([])
+  context "Acceptance tests" do
+    it "reports no anagrams" do
+      word_list = %w(xaviuzz batu gerard)
+      result = Anagrameitor.anagrams_in word_list
+      expect(result).to eq([])
+    end
+
+    it "reports anagrams" do
+      word_list = %w(ac b ca)
+      result = Anagrameitor.anagrams_in word_list
+      expect(result).to eq([['ac', 'ca']])
+    end
   end
 
-  it "reports anagrams" do
-    word_list = %w(ac b ca)
-    result = Anagrameitor.anagrams_in word_list
-    expect(result).to eq([['ac', 'ca']])
+  context "Unit test" do
+
+    describe Anagrameitor::Word do
+      it "#anagram?" do
+        a_word = described_class.new "kinship"
+        another_word = described_class.new "pinkish"
+        expect(a_word.anagram? another_word).to be true
+
+        yet_another_word = described_class.new "aaaaaaa"
+        expect(a_word.anagram? yet_another_word).to be false
+      end
+    end
   end
 
 end
