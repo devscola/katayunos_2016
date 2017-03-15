e453e7316c7261294aa442d16dac1c92c413ebf7
Eduardo Sebastian <esebastian@altiora.es>
2017-03-15 17:57:39 +0100

    Refactor: decompose expression

--- diamond/diamond.rb
+++ diamond/diamond.rb
@@ -2,7 +2,11 @@ def diamond_for(letter)
   if letter == 'A'
     "#{letter}\n"
   elsif letter == 'B'
-    " A\n#{letter} #{letter}\n A\n"
+    first_half = " A \n#{letter}"
+    gap = " "
+    second_half = first_half.reverse
+    result = first_half + gap + second_half
+    result.split("\n").map{ |l| l.rstrip }.join("\n") + "\n"
   else
     "  A\n B B\n#{letter}   #{letter}\n B B\n  A\n"
   end
