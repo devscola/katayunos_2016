e3441906082b4838d23c7c4d067ec4853774a9fa
Eduardo Sebastian <esebastian@altiora.es>
2017-03-15 18:07:17 +0100

    Refactor: extract class

--- diamond/diamond.rb
+++ diamond/diamond.rb
@@ -2,29 +2,33 @@ def diamond_for(letter)
   if letter == 'A'
     "#{letter}\n"
   elsif letter == 'B'
-    first_half = build_first_half_for(letter)
-    gap = build_gap
-    second_half = build_second_half_for(letter)
+    builder = DiamondBuilder.new(letter)
 
-    build_diamond(first_half, gap, second_half)
+    build_diamond(builder.first_half, builder.gap, builder.second_half)
   else
     "  A\n B B\n#{letter}   #{letter}\n B B\n  A\n"
   end
 end
 
-def build_first_half_for(letter)
-  " A \n#{letter}"
-end
-
-def build_gap
-  " "
-end
-
-def build_second_half_for(letter)
-  build_first_half_for(letter).reverse
-end
-
 def build_diamond(first_half, gap, second_half)
   result = first_half + gap + second_half
   result.split("\n").map{ |l| l.rstrip }.join("\n") + "\n"
+end
+
+class DiamondBuilder
+  def initialize(letter)
+    @letter = letter
+  end
+
+  def first_half
+    " A \n#{@letter}"
+  end
+
+  def gap
+    " "
+  end
+
+  def second_half
+    first_half.reverse
+  end
 end
