8f88e4487fcafcd1f2f7a171a1e07b1e61221f54
Eduardo Sebastian <esebastian@altiora.es>
2017-03-28 21:37:39 +0200

    Refactor: extract class

--- diamond/diamond.rb
+++ diamond/diamond.rb
@@ -4,7 +4,6 @@ def diamond_for(letter)
 end
 
 class DiamondBuilder
-  SPACE = " "
   NEW_LINE = "\n"
 
   def initialize(letter)
@@ -36,27 +35,52 @@ class DiamondBuilder
   end
 
   def build_line_for(current_letter)
-    padding = padding_for(current_letter)
-    padding + body_for(current_letter) + padding
-  end
-
-  def body_for(current_letter)
-    return current_letter if current_letter == 'A'
-    gap = gap_for(current_letter)
-    current_letter + gap + current_letter
-  end
-
-  def gap_for(current_letter)
-    gap_width = 2 * (current_letter.ord - 'A'.ord) - 1
-    SPACE * gap_width
-  end
-
-  def padding_for(current_letter)
-    padding_width = @letter.ord - current_letter.ord
-    padding = SPACE * padding_width
+    line_builder = LineBuilder.new(@letter, current_letter)
+    line_builder.build
   end
 
   def remove_trailing_spaces_from(lines)
     lines.map{ |line| line.rstrip }
+  end
+
+  class LineBuilder
+    SPACE = " "
+    INITIAL_LETTER = 'A'
+
+    def initialize(letter, current_letter)
+      @letter = letter
+      @current_letter = current_letter
+    end
+
+    def build
+      padding + body + padding
+    end
+
+    private
+
+    def padding
+      SPACE * padding_width
+    end
+
+    def padding_width
+      @letter.ord - @current_letter.ord
+    end
+
+    def gap
+      SPACE * gap_width
+    end
+
+    def gap_width
+      2 * (@current_letter.ord - INITIAL_LETTER.ord) - 1
+    end
+
+    def body
+      return INITIAL_LETTER if edge_line?
+      @current_letter + gap + @current_letter
+    end
+
+    def edge_line?
+      @current_letter == INITIAL_LETTER
+    end
   end
 end
